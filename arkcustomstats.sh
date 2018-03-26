ddd4dd1ea7090e779aff73b71c4611f1ea0ce0da
Ekagrah <ekagrah@icloud.com>
2018-03-25 23:12:24 -0600

    Reorder stats to be displayed

--- arkcustomstats.sh
+++ arkcustomstats.sh
@@ -8,21 +8,16 @@ echo -e "\nStats as of $(\date '+%F-%R') running map: ${map}" >> ${DAILY}
 echo -e "\n" >> ${DAILY}
 python3 /opt/bin/rcon_client_v2.py listplayers >> ${DAILY}
 
+top -b -n 1 | awk 'BEGIN {}
+FNR <= 7
+/ShooterG/{print}
+' >> ${DAILY}
+echo -e "\n" >> ${DAILY}
 \iostat -N -m >> ${DAILY}
 echo -e "\n" >> ${DAILY}
 \df -h --exclude-type=tmpfs --total >> ${DAILY}
 echo -e "\n" >> ${DAILY}
 \du -h /opt --max-depth=1 >> ${DAILY}
-echo -e "\n" >> ${DAILY}
-#top -b -n 1 | awk -f /home/derekk/.awktop.awk >> ${DAILY}
-# execute top then press E (shift+e)
-# to change unit totals are displayed 
-# in, press e to change for tasks. When set as desired press W
-# (shift+w) to save in ~/.toprc
-top -b -n 1 | awk 'BEGIN {}
-FNR <= 7
-/ShooterG/{print}
-' >> ${DAILY}
 #\ps -efH --sort=+ppid | awk '$8 !~ /^\[/||/volume-manager$/' | \grep -v "lightdm" >> ${DAILY}
 mail -s "ArkServPriv report as of $(\date '+%F-%R')" servmana < "${DAILY}"
 
