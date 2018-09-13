2e89631feb96d4f819ac24b2f4cb279c4e29ca2f
Ekagrah <ekagrah@icloud.com>
2018-09-12 23:27:16 -0600

    minor text changes
    Specifically $HOME

--- arkserv_mgmt.sh
+++ arkserv_mgmt.sh
@@ -3,37 +3,43 @@
 ## https://steamdb.info/app/376030
 
 ############
-# User editable section
+## User editable section
 PYVERS="python3"
 PYRCON='/opt/bin/rcon_client_v2.py'
 arkdir='/opt/game'
-savedir="/home/$USER/Documents/arksavedata/"
-map="TheIsland"
-#map="TheCenter"
+savedir="$HOME/Documents/arksavedata"
+#map="TheIsland"
 #map="ScorchedEarth_p"
-#map="Ragnarok"
 #map="Aberration_P"
-#map="_P"
-nplayers="10"
+#map="Extinction_P"
+#map="TheCenter"
+#map="Ragnarok"
+map="skiesofnazca"
+
+nplayers="5"
 serv_port="7777"
 query_port="27015"
 rcon_active="True"
-rcon_port="27020"
-#optional email or mail alias
-EMAIL="example@gmail.com"
+rcon_port="32330"
+##optional email or mail alias
+EMAIL="servmana"
 ############
 
 if [[ ! -e ${PYRCON} ]] ; then echo "Value for PYRCON for the python rcon tool does not seem to exist" ; exit 2 ; fi
+if [[ ! -e ${map} ]] ; then echo "Need a value for \$map" ; exit 2 ; fi
 serv_port_a="$(( ${serv_port} + 1 ))"
+script_dir="$( cd "$( dirname "$(readlink -f "$0")" )" && pwd )"
 
 USAGE () {
 echo -e "\nUsage: $0 <option>
 \tFor the time being I am only accepting one per run:
 \tstart\tStarts the game server
 \tstop\tStops game server
+\trestart\tRestarts game server
 \tmonitor\tReports back if server is running and accessible
 \tupdate\tChecks for update to dedicated server
-\tcleanup\tRemoves unnecessary mod content
+\t-uo\tDoes server update only
+\tcleanup\tRemoves unnecessary mod content               
 \t-s\tMakes a copy of server config, map save data, and player data files
 \t-h\tPrints this usage statement"
 }
@@ -54,7 +60,7 @@ done
 fnc_monitor () {
 local chkservup='false'
 local servstatus='down'
-local upcounter=12
+local upcounter=15
 if [[ $( \pgrep -x ShooterGameServ 2>/dev/null) = '' ]]; then
 	echo -e "Server does not seem to be running..."
 	exit 3
@@ -66,7 +72,7 @@ until [[ "${chkservup}" == 'true' ]]; do
 		echo "Server not ready yet, manually monitor status..."
 		exit 3
 	fi
-	#check that the final port is up
+	##check that the final port is up
 	while read -r line ; do case "$line" in udp*:${serv_port_a}*) export servstatus='up' ;; esac ; done < <(\netstat -puln 2>/dev/null | \grep ShooterGame)
 	if [[ "${servstatus}" == 'up' ]]; then 
 	echo "Server is ready"
@@ -80,11 +86,11 @@ done
 fnc_update () {
 local tmpfile="$(mktemp)"
 if [[ ${do_update} == true ]] ; then 
-# this is slow and times out occasionally
+## this is slow and times out occasionally
 	local upd_timeout=5
 	while true ;do
 	/usr/games/steamcmd +login anonymous +force_install_dir "${arkdir}" +app_update 376030 public validate +quit | tee ${tmpfile}
-# so we must verify it completed
+## so we must verify it completed
 	upd_state="$( \tail -5 ${tmpfile} | \awk -F " " '/.*App.*376030.*/{print $1}' )"
 	case $upd_state in
 		*[Ss]uccess*) echo "${upd_state} Ark server is up-to-date" ; break ;;
@@ -104,10 +110,11 @@ fi
 }
 
 fnc_chkupdate () {
-#fix for conflicting file that can prevent 
-#getting the most recent version
+##fix for conflicting file that can prevent 
+##getting the most recent version
 if [[ -e ${HOME}/.steam/steam/appcache/appinfo.vdf ]]; then rm ${HOME}/.steam/steam/appcache/appinfo.vdf ; fi
-new_vers="$( /usr/games/steamcmd +login anonymous  +app_info_update 1 +app_info_print 376030 +quit | \grep -A5 "branches" | \awk -F '"' '/buildid/{print $4}' )"
+## See if update is available
+new_vers="$( /usr/games/steamcmd +login anonymous  +app_info_update 1 +app_info_print 376030 +quit | grep -A5 "branches" | \awk -F '"' '/buildid/{print $4}' )"
 curr_vers="$( \awk -F '"' '/buildid/{print $4}' ${arkdir}/steamapps/appmanifest_376030.acf )"
 if [[ ${new_vers} -gt ${curr_vers} ]]; then
 do_update=true
@@ -123,10 +130,32 @@ if [[ ! -d ${savedir} ]] ; then
 else
 	case "${savedir}" in
 	/etc*|/bin*|/cgroup*|/lib*|/misc*|/net*|/proc*|/sbin*|/var*|/boot*|/dev*|/lib64*|/selinux*|/sys*|/usr*) echo "Not wise to save to "${savedir}"" ; exit 6 ;; esac
+	cd ${savedir}
+fi
+
+save_file=''
+case $map in
+skiesofnazca)
+save_file='SkiesofNazca.ark'
+;;
+*)
+save_file="${map}.ark"
+;;
+esac
+if [[ $(( $(\date +%s) - $(\stat -c %Y "${arkdir}"/ShooterGame/Saved/SavedArks/${save_file}) )) -lt 180 ]]; then
+	echo "World recently saved"
+else
+	${PYVERS} ${PYRCON} saveworld
+	sleep 5
+	if [[ $(( $(\date +%s) - $(\stat -c %Y "${arkdir}"/ShooterGame/Saved/SavedArks/${save_file}) )) -lt 180 ]]; then
+	echo "Ran saveworld command"
+	else
+	echo -e "Unable to verify world was recently saved, exiting..."
+	exit 2
+	fi
 fi
 local curr_date="$( \date +%b%d_%H-%M )"
-local tar_dir="tmp.${curr_date}"
-cd "${savedir}"
+local tar_dir="${map%_P}-${curr_date}"
 if [[ ! -d "${tar_dir}" ]] ; then \mkdir -p "${tar_dir}" ; fi
 echo "Copying files to ${savedir}/${tar_dir}..."
 cp "${arkdir}"/ShooterGame/Saved/Config/LinuxServer/Game.ini "${tar_dir}"/
@@ -136,8 +165,8 @@ cp "${arkdir}"/ShooterGame/Saved/SavedArks/"$map"_AntiCorruptionBackup.bak "${ta
 cp "${arkdir}"/ShooterGame/Saved/SavedArks/*.arkprofile "${tar_dir}"/
 cp "${arkdir}"/ShooterGame/Saved/SavedArks/*.arktribe "${tar_dir}"/
 echo "Making tarball..."
-\tar -czf ark-"${curr_date}".tar ./"${tar_dir}"
-rm -rf ./${tar_dir}
+\tar -czf ark-"${map}"-"${curr_date}".tar ./"${tar_dir}" && rm -rf ./${tar_dir} || echo "Unable to make tarball..."
+cd ${script_dir}
 }
 
 checkplayers () {
@@ -167,13 +196,15 @@ tmux send-keys C-c -t arkserver
 local downcounter=24
 until [[ $( \pgrep -x ShooterGameServ 2>/dev/null) == '' ]]; do 
 if [[ $downcounter -eq 0 ]]; then
-	#clear any existing tmux session
+	##clear any existing tmux session
 	tmux kill-session -t arkserver
 	##forcefully kill process for dedicated server
+	echo -e "Forcefully killing server process"
 	for i in $( \pgrep -x ShooterGameServ 2>/dev/null); do kill -9 $i; done
 	sleep 5
 	break
 fi
+## wait 2 minutes for graceful shutdown
 echo "Waiting on server to go down gracefully"
 sleep 5
 let downcounter-=1
@@ -181,7 +212,7 @@ done
 if [[ $( \pgrep -x ShooterGameServ 2>/dev/null) -eq '' ]]; then
 echo 'Successfully exited server' > /dev/null
 else
-echo 'Server does not seem to have stopped' | mail -s "Notice: Ark server" ${EMAIL}
+echo 'Server does not seem to have stopped' | mail -s "Notice: Ark server"
 exit 5
 fi
 }
@@ -192,8 +223,17 @@ ${PYVERS} ${PYRCON} broadcast Server going down for maintinence in 3 minutes.
 
 checkplayers
 
-##if the last modification/save of map is less than 3 min
-if [[ $(( $(\date +%s) - $(\stat -c %Y "${arkdir}"/ShooterGame/Saved/SavedArks/${map}.ark) )) -lt 180 ]]; then
+##Check if the last save of map is less than 3 min
+save_file=''
+case $map in
+skiesofnazca)
+save_file='SkiesofNazca.ark'
+;;
+*)
+save_file="${map}.ark"
+;;
+esac
+if [[ $(( $(\date +%s) - $(\stat -c %Y "${arkdir}"/ShooterGame/Saved/SavedArks/${save_file}) )) -lt 180 ]]; then
 	echo "World recently saved."
 	downserver
 	upserver
@@ -201,7 +241,7 @@ else
 	${PYVERS} ${PYRCON} saveworld
 	echo -e "Manually ran saveworld command..."
 	sleep 5
-	if [[ $(( $(\date +%s) - $(\stat -c %Y "${arkdir}"/ShooterGame/Saved/SavedArks/${map}.ark) )) -lt 180 ]]; then
+	if [[ $(( $(\date +%s) - $(\stat -c %Y "${arkdir}"/ShooterGame/Saved/SavedArks/${save_file}) )) -lt 180 ]]; then
 		downserver
 		upserver
 	else
@@ -216,10 +256,12 @@ fnc_updmod_conf () {
 ## This function will add mods from GameUserSettings to be auto-managed
 ## To make this work ensure the "-automanagedmods" is on commandline
 if [[ "$( \grep -o '\[ModInstaller\]' "${arkdir}"/ShooterGame/Saved/Config/LinuxServer/Game.ini )" = '' ]] ; then
-	\echo '[ModInstaller]' >> "${arkdir}"/ShooterGame/Saved/Config/LinuxServer/Game.ini
+	echo '[ModInstaller]' >> "${arkdir}"/ShooterGame/Saved/Config/LinuxServer/Game.ini
 else
 	echo 'ok' >/dev/null
 fi
+## Add each item listed with ActiveMods
+## to Game.ini for auto-patching
 for i in $( \sed -n 's/ActiveMods=//p' ${arkdir}/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini | \awk -v FS="," '{OFS=" "; $1=$1; print $0}' ) ; do
 	if [[ $(\grep -o "ModIDS=${i}" "${arkdir}"/ShooterGame/Saved/Config/LinuxServer/Game.ini) = '' ]]; then
 	echo -e "\tAdding ${i} to Game.ini"
@@ -239,21 +281,24 @@ exit 7
 fi
 local active_mods="$(\sed -n 's/ActiveMods=//p' ${arkdir}/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini | \awk -v FS="," '{OFS=" "; $1=$1; print $0}')"
 declare -a mod_list
+## find existing mod files
 while IFS=  read -r -d $'\0'; do mod_list+=("$REPLY") ; done < <(\find ./ -name '*.mod' -print0 | \sed -e 's|./111111111.mod||' -e 's|./||g' -e 's|.mod||g')
 local arr_id=0
 for m in $(\echo ${mod_list[@]} ) ; do
+## if existing file has match under ActiveMods
+## then remove it from array
 	if [[ $(\echo ${active_mods} | \grep -o ${m}) == "${m}" ]]; then
 		unset mod_list[${arr_id}]
 	else
 		echo "Marking ${m} for removal" 
 	fi
-	#this is related to the auto-mod updating that is broken
+	##this is related to the auto-mod updating that is broken
 	#\sed -i "/${m}/d" "${arkdir}"/ShooterGame/Saved/Config/LinuxServer/Game.ini
 	let arr_id++
 done
 unset arr_id
 if [[ ${#mod_list[@]} -gt 0 ]] ; then
-for d in $(echo ${mod_list[@]}) ; do
+for d in $(\echo ${mod_list[@]}) ; do
 	echo "Deleting data for mod: ${d}"
 	rm -rf ./${d}
 	rm -f ./${d}.mod
@@ -273,16 +318,17 @@ if [[ $# -gt 1 ]]; then
 fi
 
 case $1 in
-	-s) fnc_dosave ;;
-	-h) USAGE ;;
 	start) upserver ; fnc_monitor ;;
 	stop) checkplayers ; downserver ;;
+	restart) fnc_restart ; fnc_monitor ;;
 	monitor) fnc_monitor ;;
 	update) fnc_chkupdate ; fnc_update ; fnc_restart ; fnc_monitor ;;
-	restart) fnc_restart ; fnc_monitor ;;
+	-uo) fnc_chkupdate ; fnc_update	;;
 	modconf) echo "Waiting for this functionality to be fixed by WC" ; USAGE ;;
 		#fnc_updmod_conf ; fnc_restart ; fnc_monitor ;;
 	cleanup) fnc_cleanup ;;
+	-s) fnc_dosave ;;
+	-h|help) USAGE ;;
 	*) USAGE ; exit 1 ;;
 esac
 
